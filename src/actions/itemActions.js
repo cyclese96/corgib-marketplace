0a8c04175ed19fdbacb283546cb877bae0f9adb3
Tahir Ahmad <tahirahmad.in@gmail.com>
2021-08-27 21:05:47 +0530

    code optimisation

--- src/actions/itemActions.js
+++ src/actions/itemActions.js
@@ -1,36 +1,29 @@
-import { ADD_ITEM, LOAD_ITEMS, LOAD_ITEMS_ERROR } from "./types";
+import { ADD_ITEM, LOAD_ITEMS } from "./types";
+import baseUrl from "./baseUrl";
 import axios from "axios";
 
 export const loadItems = () => async (dispatch) => {
   try {
-    // FETCH DATA FROM BACKEND AND DISPATCH IT TO REDUCER
-    const api_end_point = `http://localhost:3001/items`;
-    const response = await axios.get(api_end_point);
+    const url = `${baseUrl}/items`;
+    const response = await axios.get(url);
     dispatch({
       type: LOAD_ITEMS,
       payload: response.data,
     });
   } catch (error) {
-    console.log("get data ", error);
-    dispatch({
-      type: LOAD_ITEMS_ERROR,
-    });
+    console.log(error);
   }
 };
 
 export const createItem = (itemData) => async (dispatch) => {
   try {
-    // FETCH DATA FROM BACKEND AND DISPATCH IT TO REDUCER
-    const api_end_point = `http://localhost:3001/item`;
-    const response = await axios.post(api_end_point, itemData);
+    const url = `${baseUrl}/items`;
+    const response = await axios.post(url, itemData);
     dispatch({
       type: ADD_ITEM,
       payload: response.data,
     });
   } catch (error) {
-    console.log("get data ", error);
-    dispatch({
-      type: LOAD_ITEMS_ERROR,
-    });
+    console.log(error);
   }
 };
